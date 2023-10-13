0091485a16d27f60d7c24e48b5272c1b6b717370
Jamie Gilbert <JGilber2@its.jnj.com>
2023-10-13 08:19:57 -0700

    Changed environment variables to match server settings

--- app.R
+++ app.R
@@ -6,12 +6,15 @@ resultDatabaseSettings <- createDefaultResultDatabaseSettings(
   schema = Sys.getenv("RESULT_DATABASE_SCHEMA")
 )
 
+cli::cli_h1("Starting shiny server")
+server <- Sys.getenv("howoftendbServer")
+cli::cli_alert_info("Connecting to {server}")
 connectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "postgresql",
-  server = Sys.getenv("CD_SERVER"),
-  port = Sys.getenv("CD_PORT"),
-  user = Sys.getenv("CD_USER"),
-  password = Sys.getenv("CD_PASSWORD")
+  server = Sys.getenv("howoftendbServer"),
+  port = 5432,
+  user = Sys.getenv("howoftendbUser"),
+  password = Sys.getenv("howoftendbPw")
 )
 createShinyApp(config = config,
                connectionDetails = connectionDetails,
